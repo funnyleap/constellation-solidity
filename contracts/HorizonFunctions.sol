65e69fbb09b1125ed5ed8650bf5f9ba5abea7505
funnyleap <guru1628823.tts@gmail.com>
2023-11-19 19:58:17 -0300

    Functions-complete-working

--- contracts/HorizonFunctions.sol
+++ contracts/HorizonFunctions.sol
@@ -28,10 +28,7 @@ contract GettingStartedFunctionsConsumer is FunctionsClient, ConfirmedOwner {
 
     // Event to log responses
     event Response(
-        bytes32 indexed requestId,
-        string character,
-        bytes response,
-        bytes err
+        bytes32 indexed requestId, bytes response, bytes err
     );
 
     struct VehicleData {
@@ -42,68 +39,48 @@ contract GettingStartedFunctionsConsumer is FunctionsClient, ConfirmedOwner {
 
     mapping(bytes32 requestId => VehicleData) public vehicleDataMapping;
 
-    // Router address - Modified -> Fuji
+
+    // Router address - Hardcoded for Mumbai
     // Check to get the router address for your supported network https://docs.chain.link/chainlink-functions/supported-networks
-    address router = 0xA9d587a00A31A52Ed70D6026794a8FC5E2F5dCb0;
+    address router = 0x6E2dc0F9DB014aE19888F539E59285D2Ea04244C;
+
+    //Callback gas limit
+    uint32 gasLimit = 300000;
+
+    // donID - Hardcoded for Mumbai
+    // Check to get the donID for your supported network https://docs.chain.link/chainlink-functions/supported-networks
+    bytes32 donID =
+        0x66756e2d706f6c79676f6e2d6d756d6261692d31000000000000000000000000;
+
+    uint64 private subscriptionId;
 
     // JavaScript source code
     // Fetch vehicle name from the Star Wars API.
     // Documentation: https://swapi.dev/documentation#people
     string source =
-        "const codigoTipoVeiculo = args[0];"
-        "const anoModelo = args[1];"
-        "const modeloCodigoExterno = args[2];"
+        "const id = args[0];"
         "const apiResponse = await Functions.makeHttpRequest({"
-            "url: `https://fipeapi.appspot.com/api/1/carros/veiculo/21/${vehicleId}/`"
-            "headers: {"
-                "Host: veiculos.fipe.org.br,"
-                "Referer: http://veiculos.fipe.org.br,"
-                "Content-Type: application/json"
-            " },"
-            "body: JSON.stringify({"
-                "codigoTabelaReferencia': 263,"
-                "codigoTipoVeiculo': 2,"
-                "anoModelo': 2015,"
-                "modeloCodigoExterno': '810052-7',"
-                "codigoTipoCombustivel': 1,"
-                "tipoConsulta': 'codigo'"
-            "})"
+            "url: `https://parallelum.com.br/fipe/api/v1/motos/marcas/77/modelos/5223/anos/${id}`"
         "});"
         "if (apiResponse.error) {"
         "throw Error('Request failed');"
         "}"
         "const { data } = apiResponse;"
-        // "const simplifiedResponse = `${data.Valor}|${data.CodigoFipe}|${data.Autenticacao}`;"
         "return Functions.encodeString(data.Valor);";
-
-    //Callback gas limit
-    uint32 gasLimit = 300000;
-
-    // donID - Modified -> Fuji
-    // Check to get the donID for your supported network https://docs.chain.link/chainlink-functions/supported-networks
-    bytes32 donID =
-        0x66756e2d6176616c616e6368652d66756a692d31000000000000000000000000;
-
-    // State variable to store the returned vehicle information
-    string public vehicle;
 
     /**
      * @notice Initializes the contract with the Chainlink router address and sets the contract owner
      */
-    constructor() FunctionsClient(router) ConfirmedOwner(msg.sender) {}
+    constructor(uint64 _subscriptionId) FunctionsClient(router) ConfirmedOwner(msg.sender) {
+        subscriptionId = _subscriptionId;
+    }
 
-    /**
-     * @notice Sends an HTTP request for vehicle information
-     * @param subscriptionId The ID for the Chainlink subscription
-     * @param args The arguments to pass to the HTTP request
-     * @return requestId The ID of the request
-     */
-    function sendRequest(
-        uint64 subscriptionId,
-        string[] calldata args
-    ) external onlyOwner returns (bytes32 requestId) {
+
+    function sendRequest(string[] calldata args) external onlyOwner returns (bytes32 requestId) {
         FunctionsRequest.Request memory req;
+
         req.initializeRequestForInlineJavaScript(source); // Initialize the request with JS code
+
         if (args.length > 0) req.setArgs(args); // Set the arguments for the request
 
         // Send the request and store the request ID
@@ -113,6 +90,14 @@ contract GettingStartedFunctionsConsumer is FunctionsClient, ConfirmedOwner {
             gasLimit,
             donID
         );
+        
+        VehicleData memory vehicleInfo = VehicleData ({
+            value: "",
+            requestTime: block.timestamp,
+            responseTime: 0
+        });
+
+        vehicleDataMapping[s_lastRequestId] = vehicleInfo;
 
         return s_lastRequestId;
     }
@@ -123,20 +108,20 @@ contract GettingStartedFunctionsConsumer is FunctionsClient, ConfirmedOwner {
      * @param response The HTTP response data
      * @param err Any errors from the Functions request
      */
-    function fulfillRequest(
-        bytes32 requestId,
-        bytes memory response,
-        bytes memory err
-    ) internal override {
+    function fulfillRequest( bytes32 requestId, bytes memory response, bytes memory err) internal override {
+
         if (s_lastRequestId != requestId) {
             revert UnexpectedRequestID(requestId); // Check if request IDs match
         }
+
         // Update the contract's state variables with the response and any errors
         s_lastResponse = response;
-        vehicle = string(response);
         s_lastError = err;
 
+        vehicleDataMapping[requestId].value = string(response);
+        vehicleDataMapping[requestId].responseTime = block.timestamp;
+
         // Emit an event to log the response
-        emit Response(requestId, vehicle, s_lastResponse, s_lastError);
+        emit Response(requestId, s_lastResponse, s_lastError);
     }
 }
